bbe6de06b17baad23950c6eb6060509c080302c2
JoshuaFloresta <florestajoshua.dev@gmail.com>
2023-08-12 14:42:50 +0800

    landscape

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,80 +17,62 @@ export default function Modal({
 }) {
   return (
     <div
+      className="flex fixed top-0 left-0 z-10 w-full h-full items-center justify-evenly bg-no-repeat bg-cover"
       style={{
-        display: "flex",
-        position: "fixed",
-        top: "0",
-        left: "0",
-        zIndex: "10",
-        width: "100%",
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "space-evenly",
         backgroundImage: `url(${bg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
       }}
     >
       <div
         onClick={onClick}
-        style={{
-          position: "absolute",
-          top: "50px",
-          right: "50px",
-          borderRadius: "50%",
-          padding: "5px 10px",
-          background: "black",
-          color: "white",
-          fontWeight: "900",
-          cursor: "pointer",
-        }}
-      >
+        className="absolute top-12 right-12 rounded-full px-2 py-1 font-black cursor-pointer"  >
         X
       </div>
 
       <div>
-      <img
-        src={image}
-        alt={name}
-        style={{ filter: "drop-shadow(2px 4px 12px black" }}
-      />
+        <img
+          src={image}
+          alt={name}
+          style={{ filter: "drop-shadow(2px 4px 12px black" }}
+        />
       </div>
 
-        <div style={{display:"flex", alignItems:"center",justifyContent:"space-around",flexDirection:"column",width:"450px",height:"500px"}}>
-          <div className="stat-container-title">
-            <img src={image} alt={name} className="image-title"/>
-            <p style={{width:"180px", color:"black"}}>No. {id}</p>
-            <p>{name}</p>
-            <img src={Pokeball_black} alt="pokeball" className="pokeball-title"/>
-          </div>
+      <div
+        className="flex items-center justify-around flex-col w-[450px] h-[500px]">
+        <div className="stat-container-title">
+          <img src={image} alt={name} className="image-title" />
+          <p className="text-white">No. {id}</p>
+          <p>{name}</p>
+          <img src={Pokeball_black} alt="pokeball" className="pokeball-title" />
+        </div>
 
-          <div style={{display:"flex", width:"100%"}}>
-            <div style={{background:"#dbdbd9", textAlign:"center"}} className="state-left">
-              <p>Type</p>
-              <p>Height</p>
-              <p>Weight</p>
-            </div>
-            <div style={{background:"#ffffff"}} className="stats-right">
-              <p>{type}</p>
-              <p>{height}0 cm</p>
-              <p>{weight} lbs</p>
-            </div>
+        <div style={{ display: "flex", width: "100%" }}>
+          <div
+            className="state-left bg-white text-center"
+          >
+            <p>Type</p>
+            <p>Height</p>
+            <p>Weight</p>
           </div>
-          <div className="base-stats"> 
-            <div>
-              {statsName.map((stats) => (
-                <p className="stats">{stats}</p>
-              ))}
-            </div>
-
-            <div>
-              {stats.map((stats) => (
-                <p className="stats">{stats}</p>
-              ))}
-            </div>
+          <div className="stats-right bg-white">
+            <p>{type}</p>
+            <p>{height}0 cm</p>
+            <p>{weight} lbs</p>
           </div>
         </div>
+        <div className="base-stats">
+          <div>
+            {statsName.map((stats) => (
+              <p className="stats">{stats}</p>
+            ))}
+          </div>
+
+          <div>
+            {stats.map((stats) => (
+              <p className="stats">{stats}</p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
