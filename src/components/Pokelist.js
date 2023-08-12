e0737a3f61bbfb741f8ccd9f23d7f5224a8e1fd2
JoshuaFloresta <florestajoshua.dev@gmail.com>
2023-08-12 20:01:17 +0800

    done?

--- src/components/Pokelist.js
+++ src/components/Pokelist.js
@@ -29,23 +29,24 @@ export default function Pokelist() {
 
   return (
     <div className='app-container'>
-        <div className="pokemon-container">
-            <div className="all-container">
-             {allPokemons.map((pokemonStats) =>(
-                <PokemonCard key ={pokemonStats.name}
-                 id={pokemonStats.id.toString().padStart(3,"0")}
-                  image={pokemonStats.sprites.other
-                    ["official-artwork"].front_default}
-                     name={pokemonStats.name.replace(/^./, (str)=> str.toUpperCase())} 
-                     type={pokemonStats.types[0].type.name}
-                     weight = {pokemonStats.weight}
-                     height = {pokemonStats.height}
-                     stats={pokemonStats.stats.map((stat) => stat.base_stat).slice(0,3)}
-                     statsName={pokemonStats.stats.map((stat) =>stat.stat.name).slice(0,3)}
-                    />  
-                      ))}
-            </div>
-        </div> 
+    <div className="pokemon-container">
+        <div className="all-container">
+            {allPokemons.map((pokemonStats) => (
+                <PokemonCard
+                    key={pokemonStats.name}
+                    id={pokemonStats.id.toString().padStart(3, "0")}
+                    image={pokemonStats.sprites.other["official-artwork"].front_default}
+                    name={pokemonStats.name.replace(/^./, (str) => str.toUpperCase())}
+                    types={pokemonStats.types.map((type) => type.type.name)}
+                    weight={pokemonStats.weight}
+                    height={pokemonStats.height}
+                    stats={pokemonStats.stats.map((stat) => stat.base_stat).slice(0, 3)}
+                    statsName={pokemonStats.stats.map((stat) => stat.stat.name).slice(0, 3)}
+                />
+            ))}
+        </div>
     </div>
+</div>
+
   )
 }
