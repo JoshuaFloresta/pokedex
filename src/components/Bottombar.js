c99ac0f1dd84c51b812e663ab86fdedeaad681c0
JoshuaFloresta <florestajoshua.dev@gmail.com>
2023-08-15 02:07:35 +0800

    .

--- src/components/Bottombar.js
+++ src/components/Bottombar.js
@@ -2,29 +2,29 @@ import React from 'react';
 
 export default function Bottombar() {
   return (
-    <div className="absolute bottom-0 h-11 w-full bg-black flex flex-wrap  justify-end pr-14 cursor-pointer overflow-hidden">
+    <div className="absolute bottom-0 h-11 w-full bg-black flex flex-wrap  justify-end pr-12 cursor-pointer overflow-hidden">
       <div className="flex space-x-4 items-center mx-3">
-        <div className="rounded-full bg-white px-2 py-1 text-black text-xs ">A</div>
+        <div className="rounded-full bg-white  px-[9px] py-1 text-black text-xs ">A</div>
         <p className="text-white font-medium">See Details</p>
       </div>
 
       <div className="flex space-x-4 items-center mx-3">
-        <div className="rounded-full bg-white px-2 py-1 text-black text-xs ">X</div>
+        <div className="rounded-full bg-white  px-[9px] py-1 text-black text-xs ">X</div>
         <p className="text-white font-medium">Habitat</p>
       </div>
 
       <div className="flex space-x-4 items-center mx-3">
-        <div className="rounded-full bg-white px-2 py-1 text-black text-xs ">Y</div>
+        <div className="rounded-full bg-white px-[9px] py-1 text-black text-xs ">Y</div>
         <p className="text-white font-medium">Sort</p>
       </div>
 
       <div className="flex space-x-4 items-center mx-3">
-        <div className="rounded-full bg-white px-2 py-1 text-black text-xs ">+</div>
+        <div className="rounded-full bg-white px-[9px] py-1 text-black text-xs ">+</div>
         <p className="text-white font-medium">See Evaluation</p>
       </div>
       
       <div className="flex space-x-4 items-center mx-3">
-        <div className="rounded-full bg-white px-2 py-1 text-black text-xs ">B</div>
+        <div className="rounded-full bg-white px-[9px] py-1 text-black text-xs ">B</div>
         <p className="text-white font-medium">Back</p>
       </div>
     </div>
