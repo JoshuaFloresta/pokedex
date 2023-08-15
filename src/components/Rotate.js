b3b1c0d03d2f4b0cb46e362daa9dc9599f88a10b
JoshuaFloresta <florestajoshua.dev@gmail.com>
2023-08-15 15:29:14 +0800

    responsive test

--- src/components/Rotate.js
+++ src/components/Rotate.js
@@ -4,16 +4,16 @@ import animationData from './lottie.json';
 
 function LottieAnimation() {
   return (
-    <div className='flex flex-col justify-center min-h-screen align-middle items-center'>
+    <div className='flex flex-col justify-center min-h-screen align-middle items-center bg-black'>
       <Lottie
         animationData={animationData}
         loop={true}
         autoplay={true}
         speed={1.5}
-        style={{ width: '30vw', height: '30vh' }}
+        style={{ width: '30vw', height: '30vh', filter:'brightness(70%)' }}
       />
 
-      <h1 className='mt-12 text-lg font-medium'>Rotate your Phone</h1>
+      <h1 className='mt-12 text-lg font-medium text-white'>Rotate your Phone</h1>
     </div>
   );
 }
