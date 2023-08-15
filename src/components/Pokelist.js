796695ab1099dc1dd58bc7c4c80e98884c9e59b3
JoshuaFloresta <florestajoshua.dev@gmail.com>
2023-08-15 21:17:50 +0800

    done?

--- src/components/Pokelist.js
+++ src/components/Pokelist.js
@@ -1,49 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import './Pokelist.css';
 import PokemonCard from './Pokemoncard';
-import Rotate from './Rotate'
+import axios from 'axios';
 
 export default function Pokelist() {
     const [allPokemons, setAllPokemons] = useState([]);
 
-    const getAllPokemons = async () => {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0");
-        const data = await res.json();
-
-        function createPokemonObject(results) {
-            results.forEach(async (pokemon) => {
-                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-                const data = await res.json();
-
-                // Check if a Pokémon with the same ID already exists
-                if (!allPokemons.some(p => p.id === data.id)) {
-                    setAllPokemons((currentList) => [...currentList, data]);
-                }
-            });
-        }
-        createPokemonObject(data.results);
-    };
-
     useEffect(() => {
+        const getAllPokemons = async () => {
+            try {
+                const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=600&offset=0");
+                const data = res.data;
+
+                const pokemonPromises = data.results.map(async (pokemon) => {
+                    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+                    return res.data;
+                });
+
+                const pokemonData = await Promise.all(pokemonPromises);
+                const sortedPokemons = pokemonData.sort((a, b) => a.id - b.id);
+                setAllPokemons(sortedPokemons);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
         getAllPokemons();
     }, []);
 
     return (
-        <div>
-            <div id="rotate">
-                <Rotate/>
-            </div>
-        
         <div className='app-container'>
             <div className="pokemon-container">
                 <div className="all-container">
                     {allPokemons.map((pokemonStats) => (
-                        <PokemonCard
-                            key={pokemonStats.id}
+                        <PokemonCard key={pokemonStats.name}
                             id={pokemonStats.id.toString().padStart(3, "0")}
                             image={pokemonStats.sprites.other["official-artwork"].front_default}
                             name={pokemonStats.name.replace(/^./, (str) => str.toUpperCase())}
-                            types={pokemonStats.types.map((type) => type.type.name)}
+                            type={pokemonStats.types[0].type.name}
                             weight={pokemonStats.weight}
                             height={pokemonStats.height}
                             stats={pokemonStats.stats.map((stat) => stat.base_stat).slice(0, 3)}
@@ -53,6 +47,5 @@ export default function Pokelist() {
                 </div>
             </div>
         </div>
-    </div>
     );
 }
